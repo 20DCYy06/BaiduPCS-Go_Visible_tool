be1d45ca1b985d8532f11832df7489409dc0601b
HallDave <szcwe126@qq.com>
2018-09-01 00:40:55 +0800

    Made_BaiduPCS-Go_Visible
    A tool that could directly download files from Baidu Net Disk easy and fast.The project was based on BaiduPCS-Go by iikira. This project made BaiduPCS-Go visible on Windows and easy to use. Based on .Net.这是一个可以更简单、快速的从百度网盘下载文件的工具.这个项目是基于iikira的BaiduPCS-Go,这个项目使BaiduPCS-Go在Windows上变得可视化以及更容易操作.基于.Net开发.

---
+++ BaiduPCS-Go可视化工具/Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BaiduPCS_Go可视化工具
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "登陆";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "显示文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(93, 41);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "下载文件";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 70);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(425, 368);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBox1_LinkClicked);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(682, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "帮助";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(421, 50);
+            this.textBox1.TabIndex = 5;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(682, 41);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "BaiduPCS-Go工具";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 41);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "登出";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(174, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "切换目录";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(174, 41);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "回根目录";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 441);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "                   ";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(443, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(345, 352);
+            this.listBox1.TabIndex = 11;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(443, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(179, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "下表文件/文件夹名称可直接点选";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 463);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(816, 502);
+            this.MinimumSize = new System.Drawing.Size(816, 502);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "BaiduPCS-Go可视化工具(百度云高速下载工具) by HallDave";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ BaiduPCS-Go可视化工具/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BaiduPCS_Go可视化工具
+{
+
+    public partial class Form1 : Form
+    {
+        int stateRoot = 0;
+        string FileName;
+        Process P = new Process();
+        Process PReader = new Process();
+        public void AddListItems(string ItemNames)
+        {
+            String line;
+            this.listBox1.Items.Clear();
+            int i=1;
+            StringReader sr = new StringReader(ItemNames);
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (stateRoot == 0)
+                {
+                    if (i > 8)
+                    {
+                        if (line.Length > 42)
+                        {
+                            line = line.Substring(41, line.Length - 41);
+                            this.listBox1.Items.Add(line);   //增加读出的内容到listbox 
+                        }
+                    }
+                }
+                if (stateRoot == 1)
+                {
+                    if (i > 7)
+                    {
+                        if (line.Length > 40)
+                        {
+                            line = line.Substring(39, line.Length - 39);
+                            this.listBox1.Items.Add(line);   //增加读出的内容到listbox 
+                        }
+                    }
+                }
+                i++;
+            }
+            this.listBox1.Items.RemoveAt(this.listBox1.Items.Count-1);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            P.StartInfo.Arguments = "login";
+            P.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            P.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.listBox1.Items.Clear();
+            P.StartInfo.FileName = "BaiduPCS-Go.exe";
+            P.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            P.StartInfo.Arguments = "config set -savedir C:/Downloads/";
+            P.Start();
+            PReader.StartInfo.RedirectStandardInput = true;
+            PReader.StartInfo.RedirectStandardOutput = true;
+            PReader.StartInfo.StandardOutputEncoding = System.Text.Encoding.UTF8;
+            PReader.StartInfo.UseShellExecute = false;
+            PReader.StartInfo.CreateNoWindow = true;
+            PReader.StartInfo.FileName = "BaiduPCS-Go.exe";
+            PReader.StartInfo.Arguments = "quota";
+            PReader.Start();
+            label1.Text = PReader.StandardOutput.ReadToEnd();
+            PReader.Close();
+            PReader.StartInfo.Arguments = "cd /";
+            PReader.Start();
+            PReader.Close();
+            PReader.StartInfo.Arguments = "ls";
+            PReader.Start();
+            PReader.StandardOutput.ReadToEnd();
+            PReader.Close();
+            PReader.StartInfo.Arguments = "ls";
+            PReader.Start();
+            richTextBox1.Text = "复制文件或文件夹名称到上面文本框点击下载即可！" + "\n" + "\n" + "下载的文件存储在C:/Downloads文件夹下。" + "\n" + PReader.StandardOutput.ReadToEnd() + "\n\n\n";
+            FileName = richTextBox1.Text;
+            AddListItems(FileName);
+            PReader.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            PReader.StartInfo.Arguments = "ls";
+            PReader.Start();
+            richTextBox1.Text = "复制文件或文件夹名称到上面文本框点击下载即可！" + "\n" + "\n" + "下载的文件存储在C:/Downloads文件夹下。" + "\n" + PReader.StandardOutput.ReadToEnd()+ "\n\n\n";
+            FileName = richTextBox1.Text;
+            AddListItems(FileName);
+            PReader.Close();
+            PReader.StartInfo.Arguments = "quota";
+            PReader.Start();
+            label1.Text = PReader.StandardOutput.ReadToEnd();
+            PReader.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            PReader.StartInfo.Arguments = "help";
+            PReader.Start();
+            richTextBox1.Text = PReader.StandardOutput.ReadToEnd() + "\n" + "  图形化工具是完全基于BaiduPCS_Go工具，由 HallDave 开发。仅供参考交流，非商业化使用。\n\n\n";
+            PReader.Close();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Process.Start("BaiduPCS-Go.exe");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            P.StartInfo.Arguments = "d " + textBox1.Text;
+            P.StartInfo.WindowStyle = ProcessWindowStyle.Normal;
+            P.Start();
+        }
+
+        private void richTextBox1_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            Process.Start(e.LinkText);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("确认退出？", "提示", MessageBoxButtons.OKCancel);
+            if (dr == DialogResult.OK) {
+                PReader.StartInfo.Arguments = "logout";
+                PReader.Start();
+                PReader.StandardInput.WriteLine("y");
+                PReader.Close();
+                MessageBox.Show("退出成功！", "提示");
+            } 
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            PReader.StartInfo.Arguments = "cd " + "/";
+            PReader.Start();
+            richTextBox1.Text = PReader.StandardOutput.ReadToEnd() + "\n" + "点按显示文件查看当前目录下文件。";
+            stateRoot = 0;
+            PReader.Close();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            PReader.StartInfo.Arguments = "cd " + textBox1.Text;
+            PReader.Start();
+            richTextBox1.Text = PReader.StandardOutput.ReadToEnd()+ "\n" + "点按显示文件查看当前目录下文件。";
+            stateRoot = 1;
+            PReader.Close();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = this.listBox1.SelectedItem.ToString();
+        }
+    }
+}
